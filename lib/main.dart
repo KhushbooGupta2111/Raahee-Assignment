38390a9816f2f55130ac2dbb8c3483ddd74addc7
Khushboo <guptakhushboo81537@gmail.com>
2022-03-16 19:15:40 +0530

    Added post page

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,16 @@ class _PostState extends State<Post> {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.add),
-              backgroundColor: Colors.blueGrey,
-            ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerDocked,
-            body: Column(children: [
+            // floatingActionButton: FloatingActionButton(
+            //   onPressed: () {},
+            //   child: Icon(Icons.add),
+            //   backgroundColor: Colors.blueGrey,
+            // ),
+            // floatingActionButtonLocation:
+            //     FloatingActionButtonLocation.centerDocked,
+            body: SafeArea(
+          child: ListView(children: [
+            Column(mainAxisAlignment: MainAxisAlignment.start, children: [
               Container(
                   height: 700,
                   width: double.infinity,
@@ -36,6 +38,10 @@ class _PostState extends State<Post> {
                       alignment: Alignment.topCenter,
                     ),
                   )),
+              SizedBox(
+                width: 10,
+                height: 20,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -45,18 +51,22 @@ class _PostState extends State<Post> {
                   ),
                   SizedBox(
                     width: 10,
+                    height: 20,
                   ),
                   Container(
                     child: Text(
                       "khushbooo_21",
                       style: TextStyle(
-                        fontSize: 28,
+                        fontSize: 30,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
+                  SizedBox(
+                    width: 80,
+                  ),
                   FavoriteButton(
-                    iconSize: 40,
+                    iconSize: 60,
                     isFavorite: true,
                     // iconDisabledColor: Colors.white,
                     valueChanged: (_isFavorite) {
@@ -68,10 +78,103 @@ class _PostState extends State<Post> {
                   ),
                   Icon(
                     Icons.send,
-                    color: Colors.white,
-                  )
+                    color: Colors.black,
+                    size: 40,
+                  ),
                 ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Lighten up, just enjoy life",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    //fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "#life #enjoyment",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/images/profile2.jpg'),
+                ),
+                trailing: Text(
+                  "45m",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                title: Text(
+                  "fizaaa2111",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                subtitle: Text(
+                  "Nice click  🙂",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/images/profile1.jpg'),
+                ),
+                trailing: Text(
+                  "1h",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                title: Text(
+                  "that_saahana",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                subtitle: Text(
+                  "Perfect 🤗",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
               )
-            ])));
+            ]),
+          ]),
+        )));
   }
 }
