4eb0026c79246d7179def5c5701a4f0b58939596
Khushboo <guptakhushboo81537@gmail.com>
2022-03-16 17:56:28 +0530

    Code refactored

---
+++ lib/Navigationbar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.white,
+      shape: CircularNotchedRectangle(),
+      notchMargin: 5,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.home_filled,
+              color: Colors.black,
+              size: 30,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+              size: 30,
+            ),
+            onPressed: () {},
+          ),
+          Stack(
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.mail,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                onPressed: () {},
+              ),
+              Positioned(
+                right: 10,
+                top: 10,
+                child: Icon(
+                  Icons.brightness_1,
+                  size: 10,
+                  color: Colors.red[900],
+                ),
+              )
+            ],
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              color: Colors.black,
+              size: 30,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,158 +1,31 @@
-import 'package:chat_application/utils/colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:favorite_button/favorite_button.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Post());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Post extends StatefulWidget {
+  const Post({Key? key}) : super(key: key);
 
+  @override
+  State<Post> createState() => _PostState();
+}
+
+class _PostState extends State<Post> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            //backgroundColor: bg_color,
-            body: SafeArea(
-                child: ListView(children: [
-              Padding(
-                padding: EdgeInsets.only(
-                  left: 30,
-                  top: 30,
-                  bottom: 50,
-                ),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            CircleAvatar(
-                              radius: 30,
-                              backgroundImage:
-                                  AssetImage('assets/images/profile.jpg'),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Container(
-                              child: Text(
-                                "khushbooo_21",
-                                style: TextStyle(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Icon(
-                              Icons.verified,
-                              size: 40,
-                              color: Colors.blue,
-                            ),
-                          ]),
-                      // SizedBox(
-                      //   height: 50,
-                      //   width: 100,
-                      // ),
-                      Container(
-                        padding: EdgeInsets.only(right: 20),
-                        child: Stack(
-                          children: [
-                            Icon(
-                              Icons.notifications_none,
-                              size: 40,
-                            ),
-                            Positioned(
-                              right: 0,
-                              child: Icon(
-                                Icons.brightness_1_sharp,
-                                color: Colors.red,
-                                size: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ]),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Stack(children: [
-                  Container(
-                      height: 700,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/images/post.jpg"),
-                          fit: BoxFit.cover,
-                          alignment: Alignment.topCenter,
-                        ),
-                      )),
-                  Positioned(
-                      bottom: 20,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                CircleAvatar(
-                                  radius: 30,
-                                  backgroundImage:
-                                      AssetImage('assets/images/profile.jpg'),
-                                ),
-                                SizedBox(
-                                  width: 15,
-                                ),
-                                Container(
-                                  child: Text(
-                                    "khushbooo_21",
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 28,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ]),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            child: Text(
-                              "Lighten up, just enjoy life",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                //fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Container(
-                            child: Text(
-                              "#life #enjoyment",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
-                        ],
-                      ))
-                ]),
-              ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
+              backgroundColor: Colors.blueGrey,
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            body: Column(children: [
               Container(
                   height: 700,
                   width: double.infinity,
@@ -162,11 +35,43 @@ class MyApp extends StatelessWidget {
                       fit: BoxFit.cover,
                       alignment: Alignment.topCenter,
                     ),
-                  ))
-            ])),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {},
-              backgroundColor: Colors.red,
-            )));
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('assets/images/profile.jpg'),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    child: Text(
+                      "khushbooo_21",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  FavoriteButton(
+                    iconSize: 40,
+                    isFavorite: true,
+                    // iconDisabledColor: Colors.white,
+                    valueChanged: (_isFavorite) {
+                      print('Is Favorite : $_isFavorite');
+                    },
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  )
+                ],
+              )
+            ])));
   }
 }
